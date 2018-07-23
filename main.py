9bdec079a29cb5c215748402675a1b8379da0aeb
victor <vfigueroapersonal@gmail.com>
2018-07-23 09:56:05 -0400

    added login features from chat app

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
+import webapp2
+import json
+import datetime
 
+from google.appengine.api import memcache
+from google.appengine.api import users
+
+
+
+class GetLoginUrlHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        result = {
+        'url' : users.create_login_url('/')
+        }
+        send_json(self, result)
+
+
+def send_json(request_handler, props):
+    request_handler.response.content_type = 'application/json'
+    request_handler.response.out.write(json.dumps(props))
+
+
+class GetUserHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        email = get_current_user_email()
+        result = {}
+        if email:
+            result['user'] = email
+        else:
+            result['error'] = 'User is not logged in.'
+        send_json(self, result)
+
+def get_current_user_email():
+    current_user = users.get_current_user()
+    if current_user:
+        return current_user.email()
+    else:
+        return None
+
+
+class GetLogoutUrlHandler(webapp2.RequestHandler):
+    def dispatch(self):
+        result = {
+        'url' : users.create_logout_url('/logout')
+        }
+        send_json(self, result)
