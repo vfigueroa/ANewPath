42b319301115c9529259bef7f9223926c7c645bd
Alex Wu <wua0905@gmail.com>
2018-07-24 10:07:18 -0400

    trying to fix bugs

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import datetime
 import logging
 import jinja2
+import os
 
 from google.appengine.ext import ndb
 from google.appengine.api import users
@@ -18,7 +19,6 @@ class GetLoginUrlHandler(webapp2.RequestHandler):
         'url' : users.create_login_url('/')
         }
         send_json(self, result)
-
 
 def send_json(request_handler, props):
     request_handler.response.content_type = 'application/json'
@@ -51,43 +51,41 @@ def get_current_user_distance():
     else:
         return None
 
+
+class LogDataHandler(webapp2.RequestHandler):
+    def post(self):
+        email = get_current_user_email()
+        if email:
+#            mpg = float(self.request.get('mpg'))
+            distance = float(self.request.get('distance'))
+#            transportation = self.request.get('way')
+#            comment = self.request.get('comment')
+            #co2_per_mile = 20.00 / mpg
+            log = Log(email=email, distance=distance, timestamp=str(datetime.datetime.now()))
+            log.put()
+            self.redirect('/report')
+        else:
+            log['error'] = 'User is not logged in.'
+
 class ViewReportHandler(webapp2.RequestHandler):
     def get(self):
         email = get_current_user_email()
         if email:
             q = Log.query(Log.email == email)
+            #print 'hi'
             log = q.get()
-            params = {'transportation': 'bike' ,'distance': log.distance}
-
-            template = JINJA_ENVIRONMENT.get_template('templates/report.html')
-            self.response.write(template.render(params))
-
-
-class LogDataHandler(webapp2.RequestHandler):
-	def dispatch(self):
-		data = {}
-
-
-class ViewReportHandler(webapp2.RequestHandler):
-	def post(self):
-		mpg = float(self.request.get('mpg'))
-		distance = float(self.request.get('distance'))
-		transportation = self.request.get('way')
-		comment = self.request.get('comment')
-		co2_per_mile = 20.00 / mpg
-#		logs data to admin page even when deployed
-		logging.info(co2_per_mile)
-#		transportation and comment are both Unicode strings, unicode strings and str can be compared and return true
-#		if transportation == 'bike':
-#			print('true')
-		print(co2_per_mile, distance, transportation, comment)
-class ViewHistoryHandler(webapp2.RequestHandler):
-	def dispatch(self):
-        result = {
-            'url' : users.create_logout_url('/logout')
-        }
-        send_json(self, result)
-
+            if log:
+                params = {'transportation': 'bike' ,'distance': log.distance}
+                template = JINJA_ENVIRONMENT.get_template('templates/report.html')
+                self.response.write(template.render(params))
+            else:
+                # redirect to the data form
+                self.redirect("/data")
+                print 'redirect to the data form'
+                pass
+        else:
+            print "redirect to login"
+            pass
 
 class Log(ndb.Model):
     email = ndb.StringProperty(required=True)
